fda96ed0b24356550cae628a3bac309706494d00
Suprememajor <ndipnobertetta@gmail.com>
2021-09-13 04:36:41 +0100

    added csrf

--- src/main/java/com/supremecorp/springsecurity/config/ApplicationSecurityConfig.java
+++ src/main/java/com/supremecorp/springsecurity/config/ApplicationSecurityConfig.java
@@ -32,6 +32,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
