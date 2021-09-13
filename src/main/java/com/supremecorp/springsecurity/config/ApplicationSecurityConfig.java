2cf7a3d10c9e229de59f184b8196c1bad5daf72b
Suprememajor <ndipnobertetta@gmail.com>
2021-09-13 14:32:01 +0100

    Jwt tested, works!!!

--- src/main/java/com/supremecorp/springsecurity/config/ApplicationSecurityConfig.java
+++ src/main/java/com/supremecorp/springsecurity/config/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.supremecorp.springsecurity.config;
 
 import com.supremecorp.springsecurity.auth.ApplicationUserService;
+import com.supremecorp.springsecurity.jwt.JwtTokenVerifier;
 import com.supremecorp.springsecurity.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,6 +40,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+                .addFilterAfter(new JwtTokenVerifier(), JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
